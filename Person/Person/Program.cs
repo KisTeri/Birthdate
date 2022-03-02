a684991c97d1866eb9b74bc45bd998c5deb06f51
KisTeri <100300118+KisTeri@users.noreply.github.com>
2022-03-02 23:49:38 +0300

    Add files via upload

---
+++ Person/Person/Program.cs
@@ -0,0 +1,11 @@
+﻿using Person1;
+var Roman = new Person();
+{
+    var date = new DateTime(1998, 08, 11);
+    Console.WriteLine("Roman - " + Roman.GetAge(date));
+}
+var Ekaterina = new Person();
+{
+    var date_1 = new DateTime(2004, 01, 14);
+    Console.WriteLine("Ekaterina - " + Ekaterina.GetAge(date_1));
+}
